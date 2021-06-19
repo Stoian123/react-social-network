20d138c41b0518bbce55f62b831be32b48e4a176
unknown <sto14n@yahoo.com>
2021-06-19 17:34:14 +0300

    add navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import './Navbar.module.css';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='page-right'>
+      <div className='navbar-wrapper'>
+        <div className="navbar">
+          <div className="navbar-logo">
+            <span className='navbar-logo-text'>SN</span>
+          </div>
+          <nav className='menu'>
+            <ul>
+              <li><a className='active' href="#">Profile</a></li>
+              <li><a href="#">Message</a></li>
+            </ul>
+            <ul>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Music</a></li>
+            </ul>
+            <ul>
+              <li><a href="#">Settings</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className='navbar-message'>
+          <span className='navbar-message-text'>First React Project</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
