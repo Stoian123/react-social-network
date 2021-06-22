195f8558eb1ccbf6f967d378db86dada6073c80e
unknown <sto14n@yahoo.com>
2021-06-22 12:27:51 +0300

    added objects with data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,42 +4,39 @@ import Users from './Users/Users';
 import Messages from './Messages/Messages'
 
 const Dialogs = () => {
-  const usersData = [
+  const users = [
     {id: 'kendall', name: 'Kendall'},
     {id: 'amanda', name: 'Amanda'},
     {id: 'john', name: 'John'},
-    {id: 'jessica', name: 'Jessica'},
+    {id: 'jessica', name: 'Jessica'}, 
     {id: 'emma', name: 'Emma'},
     {id: 'masha', name: 'Masha'},
   ];
-  const messagesData = [
+
+  const messages = [
     {id:1, message: 'Hi'},
     {id:2, message: 'How are you?'},
     {id:3, message: 'something...'},
     {id:4, message: 'something...'}
   ];
 
+  const usersElements = users
+    .map( u => <Users name={u.name} id={u.id}/>)
+
+  const messagesElements = messages
+    .map( m => <Messages message={m.message} />)
+
   return (
     <div className='dialogs wrapper'>
       <div className='dialogs-header section'>
         <h1 className='title'>Dialogs</h1>
       </div>
-
       <div className="dialogs-container section">
         <div className="dialogs-left">
-            <Users name={usersData[0].name} id={usersData[0].id}/>
-            <Users name={usersData[1].name} id={usersData[1].id}/>
-            <Users name={usersData[2].name} id={usersData[2].id}/>
-            <Users name={usersData[3].name} id={usersData[3].id}/>
-            <Users name={usersData[4].name} id={usersData[4].id}/>
-            <Users name={usersData[5].name} id={usersData[5].id}/>
+          { usersElements }
         </div>
-
         <div className="dialogs-right">
-          <Messages message={messagesData[0].message} />
-          <Messages message={messagesData[1].message} />
-          <Messages message={messagesData[2].message} />
-          <Messages message={messagesData[3].message} />
+          { messagesElements }
         </div>
       </div>
     </div>
--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -3,15 +3,17 @@ import Post from './Post/Post';
 import './ProfilePosts.css';
 
 const ProfilePosts = () => {
-  const postsData = [
+  const posts = [
     {id:1, message: 'First React Project!', likeCount:10},
     {id:2, message: 'Like pls.', likeCount:33}
   ];
 
+  const postsElements = posts
+    .map(p => <Post message={p.message} likeCount={p.likeCount}/>)
+
   return (
     <div className='profile-posts section'>
-      <Post message={postsData[0].message} likeCount={postsData[0].likeCount}/>
-      <Post message={postsData[1].message} likeCount={postsData[1].likeCount}/>
+      { postsElements }
     </div>
   )
 }
