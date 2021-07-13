5e3bb23ba562a074e54eff33be809cdf741a2cb4
unknown <sto14n@yahoo.com>
2021-07-13 20:52:14 +0300

    added rerender in index.js, deleted render.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import state from './components/redux/state';
-import { rerenderEntireTree } from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import state, { subscribe } from './components/redux/state';
+import { addPost, updateNewPostText } from './components/redux/state'
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 
+const rerenderEntireTree = (state) => {
+  ReactDOM.render (
+    <BrowserRouter>
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+    </BrowserRouter>,document.getElementById('root')
+  );
+};
+
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
